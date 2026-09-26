c511ea6444c6b7a07d12775e0e23fd8ceafeabf6
Vikramjit Singh <111748657+letsconfer@users.noreply.github.com>
2026-09-26 23:02:01 +0530

    Update master-config.js

--- master-config.js
+++ master-config.js
@@ -16,3 +16,22 @@ window.CV_MASTER_CONFIG = {
 
     // 4. Fixed homepage to language.html)
     homePage: "./language.html"   // or "../language.html" depending on your folder structure}
+
+     // 5. Favicon Setting
+        favicon: "logo1.png"          // Favicon image file path
+};
+
+// ==========================================
+// AUTOMATIC FAVICON INJECTOR
+// ==========================================
+(function() {
+    if (window.CV_MASTER_CONFIG && window.CV_MASTER_CONFIG.favicon) {
+        let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+        link.type = 'image/png';
+        link.rel = 'icon';
+        link.href = window.CV_MASTER_CONFIG.favicon;
+        if (!link.parentNode) {
+            document.head.appendChild(link);
+        }
+    }
+})();
